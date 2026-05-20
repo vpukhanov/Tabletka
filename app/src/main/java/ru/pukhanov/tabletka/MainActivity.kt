7c9f44d58fc41649393dd3e372458c2a6c2fc9db
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2026-05-20 15:39:14 +0300

    update today screen when date changes

--- app/src/main/java/ru/pukhanov/tabletka/MainActivity.kt
+++ app/src/main/java/ru/pukhanov/tabletka/MainActivity.kt
@@ -1,5 +1,9 @@
 package ru.pukhanov.tabletka
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,14 +15,18 @@ import androidx.lifecycle.ViewModelProvider
 import ru.pukhanov.tabletka.ui.screens.MedicationApp
 import ru.pukhanov.tabletka.ui.theme.TabletkaTheme
 import ru.pukhanov.tabletka.ui.viewmodel.MedicationViewModel
+import java.time.LocalDate
 
 class MainActivity : ComponentActivity() {
+    private lateinit var viewModel: MedicationViewModel
+    private var dateChangeReceiver: BroadcastReceiver? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         val repository = (application as TabletkaApplication).repository
-        val viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
+        viewModel = ViewModelProvider(this, object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
                 @Suppress("UNCHECKED_CAST")
                 return MedicationViewModel(repository) as T
@@ -32,6 +40,30 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize()
                 )
             }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.setCurrentDate(LocalDate.now())
+
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_DATE_CHANGED)
+            addAction(Intent.ACTION_TIMEZONE_CHANGED)
+        }
+        dateChangeReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                viewModel.setCurrentDate(LocalDate.now())
+            }
+        }
+        registerReceiver(dateChangeReceiver, filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        dateChangeReceiver?.let {
+            unregisterReceiver(it)
+            dateChangeReceiver = null
         }
     }
 }
